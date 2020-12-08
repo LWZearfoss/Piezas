3e9b4446dbc82ad0e2ca063c3223304f87c340d1
lwzearfoss <lwzearfoss@gmail.com>
2020-12-07 20:37:03 -0800

    Implements basic functionality for Piezas member functions.

--- Piezas.cpp
+++ Piezas.cpp
@@ -1,5 +1,6 @@
 #include "Piezas.h"
 #include <vector>
+#include <algorithm>
 /** CLASS Piezas
  * Class for representing a Piezas vertical board, which is roughly based
  * on the game "Connect Four" where pieces are placed in a column and 
@@ -13,23 +14,36 @@
  * [0,0][0,1][0,2][0,3]
  * So that a piece dropped in column 2 should take [0,2] and the next one
  * dropped in column 2 should take [1,2].
-**/
+ **/
 
 
 /**
  * Constructor sets an empty board (default 3 rows, 4 columns) and 
  * specifies it is X's turn first
-**/
+ **/
 Piezas::Piezas()
 {
+  for (int i = 0; i < 3; ++i) {
+    board.push_back(std::vector<Piece>());
+    for (int j = 0; j < 4; ++j) {
+      board[i].push_back(Blank);
+    }
+  }
+
+  turn = X;
 }
 
 /**
  * Resets each board location to the Blank Piece value, with a board of the
  * same size as previously specified
-**/
+ **/
 void Piezas::reset()
 {
+  for (int i = 0; i < 3; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      board[i][j] = Blank;
+    }
+  }
 }
 
 /**
@@ -39,19 +53,37 @@ void Piezas::reset()
  * In that case, placePiece returns Piece Blank value 
  * Out of bounds coordinates return the Piece Invalid value
  * Trying to drop a piece where it cannot be placed loses the player's turn
-**/ 
+ **/ 
 Piece Piezas::dropPiece(int column)
 {
-    return Blank;
+  Piece current = turn;
+  if (turn == X) {
+    turn = O;
+  } else {
+    turn = X;
+  }
+  if (column < 0 || column > 4) {
+    return Invalid;
+  }
+  for (int i = 0; i < 3; ++i) {
+    if (board[i][column] == Blank) {
+      board[i][column] = current;
+      return current;
+    }
+  }
+  return Blank;
 }
 
 /**
  * Returns what piece is at the provided coordinates, or Blank if there
  * are no pieces there, or Invalid if the coordinates are out of bounds
-**/
+ **/
 Piece Piezas::pieceAt(int row, int column)
 {
-    return Blank;
+  if (row < 0 || row > 3 || column < 0 || column > 4) {
+    return Invalid;
+  }
+  return board[row][column];
 }
 
 /**
@@ -62,8 +94,63 @@ Piece Piezas::pieceAt(int row, int column)
  * the most adjacent pieces in a single line. Lines can go either vertically
  * or horizontally. If both X's and O's have the same max number of pieces in a
  * line, it is a tie.
-**/
+ **/
 Piece Piezas::gameState()
 {
+  int x_max = 0, o_max = 0;
+
+  // Check horizontally
+  for (int i = 0; i < 3; ++i) {
+    int x_count = 0, o_count = 0;
+    for (int j = 0; j < 4; ++j) {
+      // Check if not over
+      if (board[i][j] == Blank) {
+        return Invalid;
+      }
+
+      if (board[i][j] == X) {
+        ++x_count;
+      } else {
+        x_count = 0;
+      }
+
+      if (board[i][j] == O) {
+        ++o_count;
+      } else {
+        o_count = 0;
+      }
+
+      x_max = std::max(x_max, x_count);
+      o_max = std::max(o_max, o_count);
+    }
+  }
+
+  // Check vertically
+  for (int j = 0; j < 4; ++j) {
+    int x_count = 0, o_count = 0;
+    for (int i = 0; i < 3; ++i) {
+      if (board[i][j] == X) {
+        ++x_count;
+      } else {
+        x_count = 0;
+      }
+
+      if (board[i][j] == O) {
+        ++o_count;
+      } else {
+        o_count = 0;
+      }
+
+      x_max = std::max(x_max, x_count);
+      o_max = std::max(o_max, o_count);
+    }
+  }
+
+  if (x_max > o_max) {
+    return X;
+  } else if (o_max > x_max) {
+    return O;
+  } else {
     return Blank;
+  }
 }
